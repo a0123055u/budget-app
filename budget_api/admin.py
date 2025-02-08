b8ff42df72316f98514979a83dd372d59e4a9562
Thiruvalluvan Murugesan <m.thiruvalluvar@gmail.com>
2025-02-08 23:20:00 +0800

    accedental commit

--- budget_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from budget_api.models import ExpensesCategory, IncomeCategory, ExpensesSubCategory, IncomeSubCategory
-
-admin.site.register(ExpensesCategory)
-admin.site.register(ExpensesSubCategory)
-admin.site.register(IncomeCategory)
-admin.site.register(IncomeSubCategory)
--- budget_api/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class BudgetApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'budget_api'
-    def ready(self):
-        import budget_api.signals   # noqa
